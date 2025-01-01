2110290231212fcd51540bffe67853545a434e92
mehediNabil <mehedinabil13@gmail.com>
2025-01-01 19:59:04 +0600

    adds server side

--- src/Components/OrderCard/OrderCard.jsx
+++ src/Components/OrderCard/OrderCard.jsx
@@ -14,7 +14,7 @@ const OrderCard = ({item}) => {
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Add To Cart</button>
+      <button className="btn btn-outline border-0 bg-slate-100 border-orange-400 border-b-4">Add To Cart</button>
     </div>
   </div>
 </div>
